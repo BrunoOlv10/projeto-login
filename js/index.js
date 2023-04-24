20b2cf41364edcc8e2535e19a9500879364a184e
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-04-24 19:24:04 -0300

    upload files

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
-(function () {
     function adicionar() {
         var dados = JSON.stringify({
             login   : document.querySelector("input[name=login]").value,
             senha   : document.querySelector("input[name=senha]").value
         });
         localStorage.setItem("storage", JSON.stringify(dados));
+        window.location.replace('dados.html');
+        // ou
+        // window.location.href = 'dados.html';
     }
-    var form = document.querySelector("form");
-    form.addEventListener("submit", function () {
-        return adicionar();
-    });
-})();
+    
+;
