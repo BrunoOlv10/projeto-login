ba2636cfcd3df92501baf098f2a2731fd9cd7271
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-04-23 22:56:41 -0300

    updates

--- js/index.js
+++ js/index.js
@@ -4,12 +4,10 @@
             login   : document.querySelector("input[name=login]").value,
             senha   : document.querySelector("input[name=senha]").value
         });
-        // tbClientes.push(dados);
         localStorage.setItem("storage", JSON.stringify(dados));
     }
     var form = document.querySelector("form");
     form.addEventListener("submit", function () {
-        // event.preventDefault(); event
         return adicionar();
     });
 })();
