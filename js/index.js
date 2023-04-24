25ba25941a6b5f1133bfda40cb33e8f514c7993f
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-04-23 22:03:06 -0300

    add files

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
-const btn = document.querySelector("#send");
+// const btn = document.querySelector("#send");
 
-btn.addEventListener("click", function (e) {
-    e.preventDefault();
-    const login = document.querySelector("#ilogin");
-    const value1 = login.value;
-    document.write("Email: "+value1);
-    const senha = document.querySelector("#isenha");
-    const value2 = senha.value;
-    document.write("Senha: "+value2);
-})
+// btn.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const login = document.querySelector("#ilogin");
+//     const value1 = login.value;
+//     document.write("Email: "+value1);
+//     const senha = document.querySelector("#isenha");
+//     const value2 = senha.value;
+//     document.write("Senha: "+value2);
+// })
+
+function index(){
+    window.location='form.html';
+}
+
+function save(){
+    window.localStorage.setItem('login', $('#ilogin').val());
+}
+
+function load(){
+    $('#output').val(window.localStorage.getItem('login'));
+}
+
+function erase(){
+    window.localStorage.removeItem('login');
+}
