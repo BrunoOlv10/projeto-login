b6c6927ae3d647e36a79820adc1ddbb7ab918a2d
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-04-26 00:43:52 -0300

    fix divs and positions

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@
             login : document.querySelector("input[name=login]").value,
             senha : document.querySelector("input[name=senha]").value
         });
-        
         localStorage.setItem("storage", JSON.stringify(dados));
         window.location.replace('dados.html');
     };
