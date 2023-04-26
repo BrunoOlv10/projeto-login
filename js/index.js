030e50d8ca679ca7fd287761422ba351540f8f85
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-04-25 23:47:49 -0300

    updates

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
             login : document.querySelector("input[name=login]").value,
             senha : document.querySelector("input[name=senha]").value
         });
+        
         localStorage.setItem("storage", JSON.stringify(dados));
         window.location.replace('dados.html');
     };
