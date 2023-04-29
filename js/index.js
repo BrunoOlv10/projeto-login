53d0c5a30006c09878bd359511d26ff2c1bd8f7b
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-04-29 01:30:12 -0300

    add cadastrar

--- js/index.js
+++ js/index.js
@@ -6,3 +6,15 @@
         localStorage.setItem("storage", JSON.stringify(dados));
         window.location.replace('dados.html');
     };
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyBvKQO59l_CAuZil1GOry0UVfvkNwsQscQ",
+        authDomain: "tela-login-19bad.firebaseapp.com",
+        projectId: "tela-login-19bad",
+        storageBucket: "tela-login-19bad.appspot.com",
+        messagingSenderId: "472437703349",
+        appId: "1:472437703349:web:ff620e61c7b47df73374fc"
+      };
+    
+      firebase.initializeApp(firebaseConfig);
+
