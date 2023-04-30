7ad82ba574b65fb1d4740264e72bd7b080296ad4
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-04-29 23:45:48 -0300

    add function login and get error

--- js/index.js
+++ js/index.js
@@ -6,3 +6,20 @@
         localStorage.setItem("storage", JSON.stringify(dados));
         window.location.replace('dados.html');
     };
+
+    function login() {
+        firebase.auth().signInWithEmailAndPassword(
+            form.email().value, form.senha().value
+        ).then(response => {
+            window.location.href = "../dados.html"; 
+        }).catch(error => {
+            alert(getErrorMessage(error));
+        });
+    }
+
+    function getErrorMessage(error) {
+        if(error.code == "auth/user-not-found") {
+            return "Usuário não encontrado";
+        }
+        return error.message;
+    }
