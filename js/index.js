310f126d3ca69604ba93d2d3670147569112cf9a
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2023-10-06 19:38:55 -0300

    fix login

--- js/index.js
+++ js/index.js
@@ -1,35 +1,64 @@
-    function adicionar() {
+    function adicionarLocalmente() {
         var dados = JSON.stringify({
-            login : document.querySelector("input[name=login]").value,
-            senha : document.querySelector("input[name=senha]").value
+            login : document.getElementById("email").value,
+            senha : document.getElementById("senha").value
         });
         localStorage.setItem("storage", JSON.stringify(dados));
         window.location.replace('dados.html');
     };
 
-    (form.senha().value)
-    if(form.senha().value.length < 7 || form.senha().value.length > 20) {
-    alert("Quantidade de caracteres incorreta")
-    return
-    }
-
-    function register(login, senha) {
-        return firebase.auth().createUserWithEmailAndPassword(login, senha);
-    }
-
-    function login() {
-        firebase.auth().signInWithEmailAndPassword(
-            form.email().value, form.senha().value
-        ).then(response => {
-            window.location.href = "../dados.html"; 
-        }).catch(error => {
-            alert(getErrorMessage(error));
-        });
-    }
-
     function getErrorMessage(error) {
         if(error.code == "auth/user-not-found") {
-            return "Usuário não encontrado";
+            return "Usuário não encontrado!";
+        }
+        if(error.code == "auth/email-already-in-use") {
+            return "Já existe um usuário cadastrado com este email!";
+        }
+        if(error.code == "auth/invalid-email") {
+            return "Email inválido!";
+        }
+        if(error.code == "auth/wrong-password") {
+            return "Senha incorreta!";
         }
         return error.message;
     }
+
+document.getElementById("logar").addEventListener("click", function() {
+    const form = document.getElementById("form");
+    const email = document.getElementById("email").value
+    const senha = document.getElementById("senha").value;
+
+    if(!form.checkValidity()) return;
+
+    firebase.auth().signInWithEmailAndPassword(email, senha)
+    .then(() => {
+        adicionarLocalmente()
+        alert("Logado com sucesso")
+        window.location.href = "dados.html"
+    })
+    .catch((error) => {
+        alert(getErrorMessage(error));
+    });
+    
+});
+
+document.getElementById("cadastrar").addEventListener("click", function() {
+    const form = document.getElementById("form");
+    const email = document.getElementById("email").value
+    const senha = document.getElementById("senha").value;
+
+    if(!form.checkValidity()) return;
+    if(senha.length < 7 || senha.length > 20) {
+        alert("Quantidade de caracteres incorreta! Crie uma senha entre 7 a 20 caracteres")
+        return
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(email, senha)
+    .then(() => {
+        alert("Usuário cadastrado com sucesso!");
+    })
+    .catch((error) => {
+        alert(getErrorMessage(error));
+    })
+    
+});
